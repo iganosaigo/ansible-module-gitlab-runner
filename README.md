c3a117b4f627546e877445543fbec12db510dfe2
IganoSaigo <an.grigoriev84@gmail.com>
2024-04-30 01:00:43 +0300

    Initial version

---
+++ README.md
@@ -0,0 +1,121 @@
+# Ansible Module: Gitlab Runner Registration
+
+This Ansible module provides a simple way to manage Gitlab Runner registration on a Linux system. The module can register, re-register and unregister from a given Gitlab Runner parameters.
+
+## Important
+
+This module uses new Gitlab Runner registration architecture. More details at [Gitlab Docs](https://docs.gitlab.com/ee/architecture/blueprints/runner_tokens/index.html#using-the-authentication-token-in-place-of-the-registration-token). It uses runner authentication token is used. **NOT** registration token which is deprecated.
+
+At present the module doesn't support changing configuration without re-registration.
+One of the reasons of that is because of Gitlab Runner. It thinks that managing config file by gitlab-runner service itself is a good idea. So when we try to manage runner instance we should keep in mind that service can add parameter at some points to configuration file. This idioma brings us to problem. How can we manage some runner idepotently with ansible? This module manages runner with some limitations with no overcomplication of module code, without bashsible and with no yaml programming though...
+
+## Configuring Gitlab Runner
+
+The following methods are supported:
+
+- Basic parameters could be configured with module opts.
+- Environ vars(see full list with 'gitlab-runner register --help')
+- Skeleton config file. See [Gitlab Runner Template](https://docs.gitlab.com/runner/register/#register-with-a-configuration-template) for details.
+
+Choose any method you like or even all of them at same time.
+
+If you need to change some config parameters you will have to re-register the instance.
+This could be done with `recreate` parameter. Also re-registration occures automatically when you changing authentication token.
+
+## Installation
+
+This module does require `toml` python module at target host. Beyond that no additional installation steps are required. Just place it to your [Ansible libs](https://docs.ansible.com/ansible/latest/reference_appendices/config.html#default-module-path) directory.
+
+## Usage Examples
+
+Simplest example:
+
+```yaml
+- name: Simple register runner
+  gitlab_runner_register:
+    api_url: "{{ gitlab_url }}"
+    token: "{{ gitlab_runner_token }}"
+    name: "{{ ansible_hostname }}"
+    executor: "docker"
+    default_image: "alpine:latest"
+```
+
+You could overwrite default global params of Gitlab Runner:
+
+```yaml
+- name: Simple register runner with global params
+  gitlab_runner_register:
+    api_url: "{{ gitlab_url }}"
+    token: "{{ gitlab_runner_token }}"
+    name: "{{ ansible_hostname }}"
+    executor: "docker"
+    default_image: "alpine:latest"
+    global_params:
+      concurrent: 10
+      check_interval: 60
+      connection_max_age: "1m"
+      session_server:
+        session_timeout: 180
+```
+
+Re-register the runner as simple as adding only one additional parameter `recreate`:
+
+```yaml
+- name: Re-register runner
+  gitlab_runner_register:
+    api_url: "{{ gitlab_url }}"
+    token: "{{ gitlab_runner_token }}"
+    name: "{{ ansible_hostname }}"
+    executor: "docker"
+    default_image: "alpine:latest"
+    recreate: true
+  tags: register
+```
+
+Or it's quite convenient to just run playbook with extra-vars: `-t register -e recreate=true`
+
+Now bellow example that uses environs:
+
+```yaml
+- name: Register runner with environments params
+  gitlab_runner_register:
+    api_url: "{{ gitlab_url }}"
+    token: "{{ gitlab_runner_token }}"
+    name: "{{ ansible_hostname }}"
+    environ_vars:
+      RUNNER_EXECUTOR: docker
+      DOCKER_CPUSET_CPUS: "1-2"
+      DOCKER_PRIVILEGED: true
+      DOCKER_MEMORY: 2000
+      DOCKER_IMAGE: "alpine:latest"
+```
+
+And you can use template:
+
+```yaml
+# Install template file and then run runner registration
+- name: Template Config
+  ansible.builtin.copy:
+    content: |
+      [[runners]]
+        [runners.docker]
+          tls_verify = false
+          image = "ruby:2.7"
+          cpuset_cpus = "0"
+          memory = "100"
+    dest: /tmp/temp.toml
+
+- name: Register Runner with template config and global params
+  gitlab_runner_register:
+    api_url: "{{ gitlab_url }}"
+    token: "{{ gitlab_runner_token }}"
+    name: "{{ ansible_hostname }}"
+    executor: docker
+    template_file: "/tmp/temp.toml"
+    global_params:
+      concurrent: 10
+      check_interval: 30
+      connection_max_age: "15m"
+      session_server:
+        session_timeout: 1800
+```
---
+++ gitlab_runner_register.py
@@ -0,0 +1,480 @@
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+DOCUMENTATION = r"""
+---
+module: gitlab_runner_register
+author:
+  - Alexander Grigoriev (@iganosaigo)
+short_description: Configure Gitlab Runner
+description:
+  - Register, unregister and re-register the Gitlab Runner instance.
+  - For now if you need to change some config parameters
+    you will have to re-register the instance.
+  - Also if you change the token then re-registration occurs as well.
+  - Note that runner authentication token is used.
+    NOT registration token which is deprecated. See bellow.
+  - Module uses new Gitlab Runner registration architecture.
+    More details at U(https://docs.gitlab.com/ee/architecture/blueprints/runner_tokens/index.html#using-the-authentication-token-in-place-of-the-registration-token)
+  - Instance could be configured with cli(i.e. module opts),
+    ENVs(see full list with 'gitlab-runner register --help')
+    and template file U(https://docs.gitlab.com/runner/register/#register-with-a-configuration-template).
+    Choose any you like or even all of them at same time.
+  - A valid Gitlab Runner authentication token is required for all operations.
+notes:
+  - There is conceptual problem with Gitlab Runner.
+    It thinks that managing config file by gitlab-runner service itself is
+    a good idea. So when we trying to manage runner instance we should keep in
+    mind that service can add parameter at some points to configuration file.
+    This idioma brings us to another problem. How we can manage that runner
+    idepotently with ansible? This module manages runner with some limitations
+    with no overcomplication of module code, without bashsible
+    and with no yaml programming though...
+
+requirements:
+  - python-toml
+
+attributes:
+  check_mode:
+    support: none
+  diff_mode:
+    support: none
+
+options:
+  api_url:
+    description:
+      - API URL of Gitlab Server
+    required: true
+    type: str
+  state:
+    description:
+      - Desired state of the provided Runner instance.
+    required: false
+    default: present
+    choices: ["present", "absent"]
+    type: str
+  name:
+    description:
+      - Name of Runner instance
+    required: true
+    type: str
+  executor:
+    description:
+      - Runner executor mode.
+      - If not specified you MUST specify value whether in template_file or whithin ENVs
+    required: false
+    type: str
+  default_image:
+    description:
+      - Default image of Runner instance.
+    required: false
+    type: str
+  global_params:
+    description:
+      - Options that could be set to override default globals of config file
+      - Applied only on instance registration. To change existing runner reregister required.
+      - You could find valid params at Gitlab Runner documentation.
+    required: false
+    type: dict
+  environ_vars:
+    description:
+      - You could set those env params to build config while registering instance.
+      - Applied only on instance registration. To change existing runner reregister required.
+      - You could view all values running 'gitlab-runner register --help' command.
+    required: false
+    type: dict
+  template_file:
+    description:
+      - Place that template to build config while registering instance.
+      - Applied only on instance registration. To change existing runner reregister required.
+      - You could find out how to make it at gitlab runner documentation.
+    required: false
+    type: str
+  recreate:
+    description:
+      - Force recreate Runner instance with specified params.
+      - If current instance already registered then it will unregister and register again with specified params.
+    required: false
+    default: false
+    type: bool
+"""
+
+EXAMPLES = r"""
+
+- name: Simple register runner
+  gitlab_runner_register:
+    api_url: "{{ gitlab_url }}"
+    token: "{{ gitlab_runner_token }}"
+    name: "{{ ansible_hostname }}"
+    executor: "docker"
+    default_image: "alpine:latest"
+
+- name: Simple register runner with global params
+  gitlab_runner_register:
+    api_url: "{{ gitlab_url }}"
+    token: "{{ gitlab_runner_token }}"
+    name: "{{ ansible_hostname }}"
+    executor: "docker"
+    default_image: "alpine:latest"
+    global_params:
+      concurrent: 10
+      check_interval: 30
+      connection_max_age: "15m"
+      session_server:
+        session_timeout: 1800
+
+- name: Re-register runner
+  gitlab_runner_register:
+    api_url: "{{ gitlab_url }}"
+    token: "{{ gitlab_runner_token }}"
+    name: "{{ ansible_hostname }}"
+    executor: "docker"
+    default_image: "alpine:latest"
+    recreate: true
+
+- name: Register runner with environments params
+  gitlab_runner_register:
+    api_url: "{{ gitlab_url }}"
+    token: "{{ gitlab_runner_token }}"
+    name: "{{ ansible_hostname }}"
+    environ_vars:
+      RUNNER_EXECUTOR: docker
+      DOCKER_CPUSET_CPUS: "1-2"
+      DOCKER_PRIVILEGED: true
+      DOCKER_MEMORY: 2000
+      DOCKER_IMAGE: "alpine:latest"
+
+# Install template file and then run runner registration
+- name: Template Config
+  ansible.builtin.copy:
+    content: |
+      [[runners]]
+        [runners.docker]
+          tls_verify = false
+          image = "ruby:2.7"
+          cpuset_cpus = "0"
+          memory = "100"
+    dest: /tmp/temp.toml
+
+- name: Register Runner with template config and global params
+  gitlab_runner_register:
+    api_url: "{{ gitlab_url }}"
+    token: "{{ gitlab_runner_token }}"
+    name: "{{ ansible_hostname }}"
+    executor: docker
+    template_file: "/tmp/temp.toml"
+    global_params:
+      concurrent: 10
+      check_interval: 30
+      connection_max_age: "15m"
+      session_server:
+        session_timeout: 1800
+"""
+
+RETURN = r"""
+changed:
+  description: Return changed for aix_filesystems actions as true or false.
+  returned: always
+  type: bool
+msg:
+  description: Return message regarding the action.
+  returned: always
+  type: str
+"""
+import os
+import re
+import tempfile
+import traceback
+from enum import Enum
+
+from ansible.module_utils.basic import AnsibleModule, missing_required_lib
+from ansible.module_utils.common.text.converters import to_text
+
+TOML_IMP_ERR = None
+try:
+    import toml
+
+    HAS_TOML = True
+except ImportError:
+    TOML_IMP_ERR = traceback.format_exc()
+    HAS_TOML = False
+
+RUNNER_CONFIG = "/etc/gitlab-runner/config.toml"
+RUNNER_ID = "/etc/gitlab-runner/.runner_system_id"
+
+
+class RunnerState(Enum):
+    RECREATED = "Recreated"
+    REGISTERED = "Registered"
+    UNREGISTERED = "Unregistered"
+    TOKEN_MISMATCH = "Token Reseted"
+    NAME_MISMATCH = "Runner Name Mismatch"
+
+
+class Runner(object):
+    def __init__(self, module: AnsibleModule):
+        self.module = module
+
+        self.state = self.module.params["state"]
+        self.api_url = self.module.params["api_url"]
+        self.token = self.module.params["token"]
+        self.name = self.module.params["name"]
+        self.executor = self.module.params.get("executor")
+        self.default_image = self.module.params["default_image"]
+        self.environ_vars = self.module.params.get("environ_vars")
+        self.global_params = self.module.params.get("global_params")
+        self.template_file = self.module.params.get("template_file")
+        self.recreate = self.module.params.get("recreate")
+        self.config_content = {}
+        self.warnings = []
+        self.current_name = None
+
+        self.command_results = {}
+
+        self.bin = self.get_binary()
+
+    def load_config_content(self):
+        """
+        Get current Gitlab Runner configuration content.
+        Return data as Dict.
+        """
+        return toml.load(RUNNER_CONFIG)
+
+    def make_start_config(self):
+        """
+        Global variables can't be managed with binary by ENVs or template file.
+        Before registering instance global variables modified here.
+        """
+        if self.global_params:
+            tmpfd, tmpfile = tempfile.mkstemp(dir=self.module.tmpdir)
+            with os.fdopen(tmpfd, "w") as f:
+                toml.dump(self.global_params, f)
+            self.module.atomic_move(tmpfile, RUNNER_CONFIG)
+
+    def verify_config_exists(self):
+        """
+        Assumed that base config file and runner_id file are
+        already added by gitlab-runner service itself.
+        """
+        try:
+            os.stat(RUNNER_CONFIG)
+            os.stat(RUNNER_ID)
+        except FileNotFoundError:
+            # We assume that gitlab-runner after start creates config
+            self.module.fail_json(
+                msg="Runner didn't initialize it's minimal config",
+            )
+
+    def get_state(self):
+        """
+        Simple mapping of different gitlab-runner states
+        """
+        try:
+            runner_section = self.config_content["runners"][0]
+            runner_name = runner_section["name"]
+            runner_token = runner_section["token"]
+        except (IndexError, KeyError):
+            return RunnerState.UNREGISTERED
+        if runner_token != self.token:
+            return RunnerState.TOKEN_MISMATCH
+        if runner_name != self.name:
+            self.warnings.append(RunnerState.NAME_MISMATCH.value)
+            self.current_name = runner_name
+
+        return RunnerState.REGISTERED
+
+    def register_runner(self):
+        """
+        Register Gitlab Runner. It runs binary in non-interactive mode.
+        If template file provided then add template path to command
+        in addition to cli parameters.
+        """
+        cmd = [self.bin, "register", "--non-interactive"]
+        cmd.extend(["--url", self.api_url])
+        cmd.extend(["--token", self.token])
+        cmd.extend(["--name", self.name])
+        if self.executor:
+            cmd.extend(["--executor", self.executor])
+        if self.default_image:
+            cmd.extend(["--docker-image", self.default_image])
+        if self.template_file:
+            cmd.extend(["--template-config", self.template_file])
+
+        # Prepare environment variables
+        environment = None
+        if self.environ_vars:
+            environment = {}
+            for k, v in self.environ_vars.items():
+                environment[k] = to_text(v)
+
+        rc, stdout, stderr = self.module.run_command(
+            cmd,
+            environ_update=environment,
+        )
+        if rc != 0:
+            self.module.fail_json(
+                msg="Error while registering runner",
+                stdout=stdout,
+                stderr=stderr,
+            )
+
+    def unregister_runner(self):
+        """
+        Unregister Gitlab Runner instance.
+        """
+        cmd = [self.bin, "unregister"]
+        name = self.name
+        if self.current_name:
+            name = self.current_name
+        cmd.extend(["--name", name])
+        rc, stdout, stderr = self.module.run_command(cmd)
+        if rc != 0:
+            self.module.fail_json(
+                msg="Error while unregistering runner",
+                stdout=stdout,
+                stderr=stderr,
+            )
+
+    def get_binary(self):
+        """
+        Get binary path of gitlab-runner.
+        """
+        return self.module.get_bin_path("gitlab-runner", required=True)
+
+    def check_service(self):
+        """
+        Verify that gitlab-runner service is running
+        """
+        cmd = [self.bin, "status"]
+        rc, stdout, stderr = self.module.run_command(cmd)
+        if rc != 0:
+            if re.search(r"Service has stopped", stderr, re.MULTILINE):
+                self.module.fail_json(msg="Service(systemd) not running.")
+            else:
+                self.module.fail_json(
+                    "gitlab-runner can't get status",
+                    stdout=stdout,
+                    stderr=stderr,
+                )
+
+        if not re.search("Service is running", stdout, re.MULTILINE):
+            self.module.fail_json(
+                msg="Gitlab-Runner service not running! Ensure service is up.",
+                stdout=stdout,
+                stderr=stderr,
+            )
+
+    def do_disable(self):
+        "Unregister Gitlab Runner."
+        self.unregister_runner()
+
+    def do_enable(self):
+        """
+        Create basic minimal config file with global variables.
+        Run registration of Gitlab Runner.
+        """
+        self.make_start_config()
+        self.register_runner()
+
+    def do_reenable(self):
+        """
+        Recreate Gitlab Runner instance.
+        It creates basic minimal config file and
+        run registration of Gitlab Runner.
+        """
+        self.unregister_runner()
+        self.do_enable()
+
+    def act(self):
+        """
+        Main logic entrypoint.
+        """
+        self.check_service()
+        self.verify_config_exists()
+        self.config_content = self.load_config_content()
+
+        state_before = self.get_state()
+        state_after = None
+        self.command_results["state_before"] = state_before.value
+        self.command_results["state_after"] = None
+
+        if self.state == "present":
+            if state_before == RunnerState.UNREGISTERED:
+                self.do_enable()
+                state_after = RunnerState.REGISTERED
+
+            elif state_before == RunnerState.TOKEN_MISMATCH:
+                self.do_reenable()
+                state_after = RunnerState.RECREATED
+
+            elif state_before == RunnerState.REGISTERED:
+                if self.recreate:
+                    self.do_reenable()
+                    state_after = RunnerState.RECREATED
+
+        elif self.state == "absent":
+            if state_before == RunnerState.REGISTERED:
+                self.do_disable()
+                state_after = RunnerState.UNREGISTERED
+
+        if state_after:
+            self.command_results["state_after"] = state_after.value
+        else:
+            self.command_results["state_after"] = state_before.value
+
+        if self.warnings:
+            self.command_results["warnings"] = self.warnings
+
+        self.command_results["changed"] = (
+            state_before.value != self.command_results["state_after"]
+        )
+        self.module.exit_json(**self.command_results)
+
+
+def main():
+    module = setup_module_object()
+    if not HAS_TOML:
+        module.fail_json(
+            msg=missing_required_lib("toml"),
+            exception=TOML_IMP_ERR,
+        )
+
+    gitlab_runner = Runner(module)
+    gitlab_runner.act()
+
+
+argument_spec = dict(
+    api_url=dict(required=True, type="str"),
+    state=dict(
+        choices=["present", "absent"],
+        default="present",
+    ),
+    token=dict(required=True, type="str", no_log=True),
+    name=dict(required=True, type="str"),
+    executor=dict(type="str"),
+    default_image=dict(type="str"),
+    global_params=dict(type="dict"),
+    environ_vars=dict(type="dict"),
+    template_file=dict(type="str"),
+    recreate=dict(type="bool", default=False),
+)
+
+
+def setup_module_object():
+    module = AnsibleModule(
+        argument_spec=argument_spec,
+        supports_check_mode=False,
+        required_one_of=[
+            [
+                "executor",
+                "template_file",
+                "environ_vars",
+            ],
+        ],
+    )
+    return module
+
+
+if __name__ == "__main__":
+    main()
